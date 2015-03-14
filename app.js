10bbc2d2998d175239fd8dcca93d9ac26796aa6a
Jimmy Farrell <jfarrell22@gmail.com>
2015-03-14 01:17:55 -0400

    Able to make requests to Spotify, SoundCloud, and YouTube

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ var app = express();
 var logger = require('morgan');
 var swig = require('swig');
 var bodyParser = require('body-parser');
+var request = require('request');
+var keys = require('./keys');
+// var SpotifyWebApi = require('spotify-web-api-node');
+// var spotifyApi = new SpotifyWebApi({
+// 	clientId : keys.spotifyId,
+//   clientSecret : keys.spotifySecret,
+//   redirectUri : 'http://localhost:2289/'
+// });
 
 app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
@@ -24,6 +32,18 @@ app.get('/', function(req, res) {
 app.post('/search', function(req, res) {
 	var artist = req.body.artist;
 	var song = req.body.song;
-	console.log(artist, song);
+	request('https://api.spotify.com/v1/search?q=' + song + ' cover' + '&type=track', function(err, res, songs) {
+		if (err) throw 'Spotify Search Error';
+		var spotifyResults = {};
+		console.log(songs);
+	});
+	request('https://api.soundcloud.com/tracks.json?consumer_key=' + keys.soundcloudId + '&q=' + song + ' ' + artist + '&order=original', function(err, res, songs) {
+		if (err) throw 'SoundCloud Search Error';
+
+	});
+	request('https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + song + ' cover' + '&key=' + keys.youtubeKey, function(err, res, videos) {
+		if (err) throw 'YouTube Search Error';
+
+	});
 	res.redirect('/');
 });
